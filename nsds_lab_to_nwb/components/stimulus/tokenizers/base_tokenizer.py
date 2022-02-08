ef2a511b691be9ef88b3032c2df1172d1abba523
John Hermiz <jthermiz@gmail.com>
2022-02-07 18:06:49 -0800

    Removes using TDT marks

--- nsds_lab_to_nwb/components/stimulus/tokenizers/base_tokenizer.py
+++ nsds_lab_to_nwb/components/stimulus/tokenizers/base_tokenizer.py
@@ -31,10 +31,11 @@ class BaseTokenizer():
         raise NotImplementedError
 
     def get_stim_onsets(self, mark_onsets, mark_time_series):
-        if mark_onsets is not None:
-            # loaded directly from TDT object
-            logger.info('Using stimulus onsets directly loaded from TDT')
-            return mark_onsets
+        # This ignore mark offset from metadata!
+        # if mark_onsets is not None:
+        #     # loaded directly from TDT object
+        #     logger.info('Using stimulus onsets directly loaded from TDT')
+        #     return mark_onsets
 
         logger.info('Detecting stimulus onsets by thresholding the mark track')
         mark_fs = mark_time_series.rate
